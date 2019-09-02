71d493a0bbfd9b25da7829a390eab1cf7bd3397d
Thiago Ruis <thiago.ruis@stormtech.com.br>
2019-09-02 03:57:13 -0300

    Beta finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MoviesAPIService} from './movies-api.service';
+import { Movie } from './movie';
+import {DetailDialogComponent} from "./detail-dialog/detail-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
+import {empty} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ruis-frontend';
-  movies = [1, 2, 3, 4, 5];
+export class AppComponent implements OnInit {
+  title = 'COisa Linda';
+  movies: Movie[];
+
+
+
+  constructor( private movieApi: MoviesAPIService, private dialog: MatDialog ) { }
+
+  ngOnInit() {
+    this.getUpcomingMovies();
+  }
+
+  getUpcomingMovies() {
+    this.movieApi.listUpcoming().subscribe((upcomingMovies: Movie[]) => {
+      this.movies = upcomingMovies;
+    });
+  }
+
+  getMovieDetail(movieId) {
+    this.movieApi.listMovieDetail(movieId).subscribe((movieDetail: Movie) => {
+      const dialogRef = this.dialog.open(DetailDialogComponent, {
+        width: '400px',
+        data: movieDetail,
+      });
+    });
+  }
+
+  searchMovie(movieName) {
+    if (movieName.value.trim() == '') {
+      this.getUpcomingMovies();
+    } else {
+      this.movieApi.searchMovie(movieName.value).subscribe((upcomingMovies: Movie[]) => {
+        this.movies = upcomingMovies;
+      });
+    }
+  }
+
+  clear(movieName) {
+    movieName.value = '';
+    this.getUpcomingMovies();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule } from "@angular/material";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from "@angular/common/http";
+import { DetailDialogComponent } from './detail-dialog/detail-dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailDialogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  entryComponents: [
+    DetailDialogComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
